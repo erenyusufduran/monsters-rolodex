bc5f8674e1579193e6a3c6f7e7f2102da726746a
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-09 20:52:02 +0300

    devidingComponenst&someCss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
 import "./App.css";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -36,19 +38,13 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          className="monster-search-box"
+          onChangeHandler={onSearchChange}
           placeholder="Search Monsters"
-          onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
