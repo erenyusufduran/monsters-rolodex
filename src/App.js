268ed1eb620da2802343387053e549428cad6d6d
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-09 17:14:28 +0300

    optimizations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-
 import "./App.css";
 
 class App extends Component {
@@ -7,7 +6,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
-      searchField: '',
+      searchField: "",
     };
   }
 
@@ -21,9 +20,19 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     return (
       <div className="App">
@@ -31,12 +40,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="Search Monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
